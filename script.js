605e0d05c0c5eef5bf5903c8de1dfb7004f14bea
moses-ka <ji.moses.ka@gmail.com>
2023-04-15 16:32:06 +0200

    added a function to delete a task

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 const Con = document.querySelector(".posts");
 
+var postNum = 1
 
 const addTask = () => {
+     postNum++;
     const task = document.getElementById("taskIn").value
-    const elementToAdd = ` <div class="post1">
+    const elementToAdd = ` <div class="post1 postNum${postNum} " >
     <div id="taskContainer" class="taskContainer">
-        <h3 id="text">${task}</h3>
+        <h3  id="text postNum${postNum}">${task}</h3>
     </div>
       <div>
-        <img class="img" width="30px" src="assets/delete.png" alt="">
+      <button name="delete" onclick="deleteTask(${postNum})" >
+      <img class="img" width="30px" src="assets/delete.png" alt="">
+  </button>
         <img class="img" width="30px" src="assets/edit.png" alt=""> 
       </div>
 </div>`
-Con.insertAdjacentHTML("afterend", elementToAdd)
+if(task != ""){
+
+  Con.insertAdjacentHTML("beforeend", elementToAdd)
+}
+}
+const  deleteTask = (x) => {
+  
+ let elementToDelete = document.querySelector(`.postNum${x}`)	;
+ elementToDelete.remove();
+
+}
+const  edit = (task) => {
+  let elementToEdit = task.replace(" ","")
+  elementToEdit.	;
+  
 }
