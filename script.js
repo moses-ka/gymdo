0af37fec9015f956cf077e82af54a24db73c0ae5
moses-ka <ji.moses.ka@gmail.com>
2023-04-29 01:50:52 +0200

    i think its done

--- script.js
+++ script.js
@@ -5,23 +5,31 @@ let retrieveData = () => {
     const key = localStorage.key(i);
     if (key.startsWith('post')) {
       const elementToAdd = localStorage.getItem(key);
-      Con.insertAdjacentHTML('beforeend', elementToAdd);
+      Con.insertAdjacentHTML('afterbegin', elementToAdd);
     }
   }
 }
 
 retrieveData();
+let taskInput = document.getElementById("taskIn");
+taskInput.addEventListener("keydown", (e) => {
+  if (e.key === 'enter' || e.key === 'Enter') {
+    addTask();
+    taskInput.input.value = ""; // not working
+  }
+});
 
 const addTask = () => {
   let postNum = Math.floor(Math.random() * 1000000);
   postNum = postNum.toString();
 
   const task = document.getElementById("taskIn").value;
-  const elementToAdd = ` <div class="post1 postNum${postNum}">
+  const dataNum = Math.floor(Math.random() * 1000000);
+  const elementToAdd = ` <div class="post1 postNum${postNum}" id="post${dataNum}">
     <div id="taskContainer" class="taskContainer">
       <h3 onclick="editTask(${postNum})" class="textNum${postNum}" id="text">${task}</h3>
     </div>
-    <div>
+    <div class="buttons">
       <button>
         <img onclick="markAsDone(${postNum})" class="img" width="30px" src="assets/verified.png" alt="">
       </button>
@@ -35,24 +43,28 @@ const addTask = () => {
     Con.insertAdjacentHTML("beforeend", elementToAdd);
   }
 
-  storeData(elementToAdd);
+  storeData(elementToAdd, `post${dataNum}`);
+ 
 }
 
-const storeData = (elementToAdd) => {
-  const dataNum = Math.floor(Math.random() * 1000000);
-  const key = `post${dataNum}`;
+
+const storeData = (elementToAdd, key) => {
   localStorage.setItem(key, elementToAdd);
   console.log(`Data stored with key ${key}`);
+  deleteTask(key);
 }
 
 const deleteTask = (x) => {
+  const dataNum = document.querySelector(`.postNum${x}`).id.slice(4);
   let elementToDelete = document.querySelector(`.postNum${x}`);
   elementToDelete.remove();
+  localStorage.removeItem(`post${dataNum}`);
+  console.log('this is the key : post' + dataNum);
+  
 }
 
 let editTask = (x) => {
   let elementToEdit = document.querySelector(`.textNum${x}`);
-  let anyWhere = document.querySelector('body');
   let input = document.createElement('input');
 
   input.type = 'text';
@@ -68,6 +80,8 @@ let editTask = (x) => {
     elementToEdit.innerHTML = input.value;
     if (input.value == '') {
       deleteTask(x);
+    } else {
+      updateTask(x, input.value);
     }
   });
 
@@ -77,13 +91,28 @@ let editTask = (x) => {
       elementToEdit.innerHTML = input.value;
       if (input.value == '') {
         deleteTask(x);
+      } else {
+        updateTask(x, input.value);
       }
     }
   });
 };
 
+const updateTask = (x, newValue) => {
+  const dataNum = document.querySelector(`.postNum${x}`).id.slice(4);
+  const key = `post${dataNum}`;
+  const elementToUpdate = localStorage.getItem(key);
+  const updatedElement = elementToUpdate.replace(/<h3.*?>(.*?)<\/h3>/, `<h3 onclick="editTask(${x})" class="textNum${x}" id="text">${newValue}</h3>`);
+  localStorage.setItem(key, updatedElement);
+};
+
 const markAsDone = (x) => {
   let elementToMarkAsDone = document.querySelector(`.textNum${x}`);
   elementToMarkAsDone.classList.toggle('done');
-  console.log(elementToMarkAsDone);
-}
+  const dataNum = document.querySelector(`.postNum${x}`).id.slice(4);
+  const key = `post${dataNum}`;
+  const elementToUpdate = localStorage.getItem(key);
+  const isDone = elementToMarkAsDone.classList.contains('done');
+  const updatedElement = elementToUpdate.replace(/<h3.*?>(.*?)<\/h3>/, `<h3 onclick="editTask(${x})" class="textNum${x} ${isDone ? 'done' : ''}" id="text">${elementToMarkAsDone.innerHTML}</h3>`);
+  localStorage.setItem(key, updatedElement);
+};
